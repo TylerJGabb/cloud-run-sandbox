efce1790e5a03e1b5744a472152781ca70592dde
Tyler Gabb <tylerjgabb@gmail.com>
2024-04-13 19:45:55 -0700

    try to extract trace

--- go.mod
+++ go.mod
@@ -6,3 +6,8 @@ require (
 	github.com/googleapis/google-cloudevents-go v0.8.0
 	google.golang.org/protobuf v1.33.0
 )
+
+require (
+	cloud.google.com/go/compute v1.14.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+)
--- main.go
+++ main.go
@@ -7,28 +7,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
+	"cloud.google.com/go/compute/metadata"
 	"github.com/googleapis/google-cloudevents-go/cloud/storagedata"
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
-/*
-TODO: build some sort of logger that extracts the tract id
+const (
+	GOOGLE_PROJECT_ID_ENV = "GOOGLE_PROJECT_ID"
+)
 
-	Derive the traceID associated with the current request.
-	var trace string
-	if projectID != "" {
-	        traceHeader := r.Header.Get("X-Cloud-Trace-Context")
-	        traceParts := strings.Split(traceHeader, "/")
-	        if len(traceParts) > 0 && len(traceParts[0]) > 0 {
-	                trace = fmt.Sprintf("projects/%s/traces/%s", projectID, traceParts[0])
-	        }
-	}
-*/
+var (
+	projectId   string
+	projectIdOk bool
+	rootLogger  Logger = Logger{}
+)
+
 type Entry struct {
 	Message  string `json:"message"`
 	Severity string `json:"severity,omitempty"`
 	Trace    string `json:"logging.googleapis.com/trace,omitempty"`
+
+	JsonPayload map[string]interface{} `json:"jsonPayload,omitempty"`
 
 	// Logs Explorer allows filtering and display of this as `jsonPayload.component`.
 	Component string `json:"component,omitempty"`
@@ -46,28 +48,54 @@ func (e Entry) String() string {
 	return string(out)
 }
 
-func Error(m string) {
-	e := Entry{Message: m, Severity: "ERROR"}
+func (l Logger) Error(m string) {
+	e := Entry{Message: m, Severity: "ERROR", Trace: l.Trace}
 	log.Println(e.String())
 }
 
-func Info(m string) {
-	e := Entry{Message: m}
+func (l Logger) Info(m string) {
+	e := Entry{Message: m, Trace: l.Trace}
 	log.Println(e.String())
 }
 
-func Warn(m string) {
-	e := Entry{Message: m, Severity: "WARNING"}
+func (l Logger) Warn(m string) {
+	e := Entry{Message: m, Severity: "WARNING", Trace: l.Trace}
 	log.Println(e.String())
+}
+
+type Logger struct {
+	Trace string
+}
+
+func NewLoggerFromRequest(req http.Request) Logger {
+	var trace string
+	if projectId != "" {
+		traceHeader := req.Header.Get("X-Cloud-Trace-Context")
+		traceParts := strings.Split(traceHeader, "/")
+		if len(traceParts) > 0 && len(traceParts[0]) > 0 {
+			trace = fmt.Sprintf("projects/%s/traces/%s", projectId, traceParts[0])
+		}
+	}
+	logger := Logger{
+		Trace: trace,
+	}
+	if trace != "" {
+		logger.Info(fmt.Sprintf("Trace: %v", trace))
+	} else {
+		logger.Warn("Trace is not set")
+
+	}
+	return logger
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
+	logger := NewLoggerFromRequest(*req)
 	sd := storagedata.StorageObjectData{}
 	bytes, err := io.ReadAll(req.Body)
-	Info(fmt.Sprintf("Received event: %v\n", string(bytes)))
+	logger.Info(fmt.Sprintf("Received event: %v", string(bytes)))
 	if err != nil {
 		errMsg := fmt.Sprintf("Failed to read request body: %v", err)
-		Error(errMsg)
+		logger.Error(errMsg)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(errMsg))
 		return
@@ -75,40 +103,54 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	defer req.Body.Close()
 	if err := protojson.Unmarshal(bytes, &sd); err != nil {
 		errMsg := fmt.Sprintf("Failed to unmarshal request body: %v", err)
-		Error(errMsg)
+		logger.Error(errMsg)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(errMsg))
 		return
-
 	}
 
-	log.Println()
-
 	// print the data
-	fmt.Printf("Bucket: %v\n", sd.Bucket)
-	fmt.Printf("Name: %v\n", sd.Name)
-	fmt.Printf("Id: %v\n", sd.Id)
+	logger.Info(fmt.Sprintf("Bucket: %v", sd.Bucket))
+	logger.Info(fmt.Sprintf("Name: %v", sd.Name))
+	logger.Info(fmt.Sprintf("Id: %v", sd.Id))
 
 	// print the contents of the file /gcs/${name}
 
 	data, err := os.ReadFile("/gcs/" + sd.Name)
 	if err != nil {
-		Error(fmt.Sprintf("Failed to read file: %v", err))
+		logger.Error(fmt.Sprintf("Failed to read file: %v", err))
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 		return
 	}
-	Info(fmt.Sprintf("Contents: %v\n", string(data)))
+	logger.Info(fmt.Sprintf("Contents: %v", string(data)))
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	log.SetFlags(0)
+	// https://cloud.google.com/run/docs/container-contract#metadata-server
+	// https://pkg.go.dev/cloud.google.com/go/compute/metadata
+	projectId, projectIdOk = os.LookupEnv(GOOGLE_PROJECT_ID_ENV)
+	if !projectIdOk {
+		rootLogger.Warn(GOOGLE_PROJECT_ID_ENV + " is not set, trying to get project ID from metadata server")
+		metadataClient := metadata.NewClient(&http.Client{
+			Timeout: time.Duration(5 * time.Second),
+		})
+		metaProj, err := metadataClient.ProjectID()
+		if err != nil {
+			errMsg := fmt.Sprintf("Failed to get project ID: %v", err)
+			rootLogger.Error(errMsg)
+			panic(err)
+		}
+		projectId = metaProj
+	}
+	rootLogger.Info(fmt.Sprintf("Project ID: %v", projectId))
 	http.HandleFunc("/", handler)
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
 		port = "8080"
 	}
-	fmt.Printf("Server starting at port %s\n", port)
+	rootLogger.Info("Server starting at port " + port)
 	panic(http.ListenAndServe(":"+port, nil))
 }
